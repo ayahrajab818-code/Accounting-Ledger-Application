6047199988135fcd2e0647ceafc492f913a04068
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-10-10 12:00:06 -0400

    not done yet

---
+++ src/main/java/com/pluralsight/homeScreen.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class homeScreen {
+    public static ArrayList<Transaction> transactions = getTransactionsFromFile();
+    public static void main(String[] args) {
+        String 
+
+
+    }
+    public static ArrayList<Transaction> getTransactionsFromFile() {
+    }
+}
