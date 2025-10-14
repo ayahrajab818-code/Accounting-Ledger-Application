96d30f27eae867184725687f98e9ffb248b0fe5a
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-10-14 09:02:30 -0400

    added comments

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -34,26 +34,6 @@ public class Transaction {
         return vendor;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setVendor(String vendor) {
-        this.vendor = vendor;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
     public double getAmount() {
         return amount;
 
